35e7ca34fb30f05beef156f1d218bb60f955a5d7
Rehnskiold850 <138557108+FloreaCostinMario@users.noreply.github.com>
2025-06-26 12:15:46 +0300

    Basic error handling due to API instability + feed update

--- FetchAndConvert.py
+++ FetchAndConvert.py
@@ -3,6 +3,7 @@ import json
 import csv
 import zipfile
 import os
+import time
 from tqdm import tqdm
 from io import StringIO
 from datetime import timedelta
@@ -14,6 +15,8 @@ if os.getenv("GITHUB_ACTIONS") != "true":
 
 API_KEY = os.getenv('TranzyGithubBuildKey') # Replace with your own API key
 BASE_URL = "https://api.tranzy.ai/v1/opendata"
+MAX_RETRY = 5
+RETRY_WAIT = 3
 
 ######################################################################################################################################
 ##                                           GTFS files are generated succesfully however                                           ##
@@ -36,7 +39,7 @@ session.headers.update({
 # Setup rate limiting
 @sleep_and_retry
 @limits(calls=4, period=timedelta(seconds=1).total_seconds())
-def FetchFromURL(URL: str, headers=None, params=None):
+def FetchFromURL(URL: str, headers=None, params=None, retry=None):
     try:
         response = session.get(URL, headers=headers, params=params)
         response.raise_for_status()
@@ -46,9 +49,20 @@ def FetchFromURL(URL: str, headers=None, params=None):
             return response.json()
         return response.content
     
+    # Error handling and retry
     except requests.exceptions.RequestException as e:
-        print(f"Request failed: {e}")
-        return None
+        if retry == MAX_RETRY:
+            print(f"\nRequest failed: {e}")
+            return None
+        else:
+            print(f"\nRequest failed: {e}, retrying in {RETRY_WAIT} seconds.")
+            if retry is None: 
+                retry = 1
+            else: 
+                retry += 1
+
+            time.sleep(RETRY_WAIT)
+            return FetchFromURL(URL, headers, params, retry)
 
 # Converting API responses to CSV/TXT
 def JsonToCSVString(data):
@@ -111,6 +125,64 @@ def FixAgency(Agency):
 
     return Agency
 
+def ProccessAgency(Agency):
+    # Temporary bypass, Oradea can't be used but is in API
+    if Agency["agency_id"] == 9: return
+
+    # Generate shapes for each trip
+    print(f"Proccesing agency: {Agency['agency_name']}")
+
+    # Fix agency URLs
+    Agency = FixAgency(Agency)
+
+    # Value to contain all csv formated strings
+    FilesToGenerate = {}
+
+    Routes = JsonToCSVString(FixRoute(FetchFromURL(f"{BASE_URL}/routes", {"X-Agency-Id" : str(Agency["agency_id"])})))
+    Trips = FetchFromURL(f"{BASE_URL}/trips", {"X-Agency-Id" : str(Agency["agency_id"])})
+    
+    # Generate shapes for each trip
+    print("Start shape generation")
+
+    Shapes = json.loads('[]')        
+    for Trip in tqdm(Trips, desc="Fetching shapes", unit="shape"):        
+        TripShape = FetchFromURL(f"{BASE_URL}/shapes", {"X-Agency-Id" : str(Agency["agency_id"])}, {"shape_id": Trip["shape_id"]})
+        Shapes += TripShape
+    
+    # Convert used jsons to csv format
+    Trips = JsonToCSVString(Trips)
+    Shapes = JsonToCSVString(Shapes)
+
+    Stops = JsonToCSVString(FetchFromURL(f"{BASE_URL}/stops", {"X-Agency-Id" : str(Agency["agency_id"])}))
+    StopTimes = JsonToCSVString(FetchFromURL(f"{BASE_URL}/stop_times", {"X-Agency-Id" : str(Agency["agency_id"])}))
+
+    # Gather files for compression
+    FilesToGenerate.update({
+        "agency.txt": JsonToCSVString(Agency),
+        "routes.txt": Routes,
+        "trips.txt": Trips,
+        "stops.txt": Stops,
+        "shapes.txt": Shapes,
+        "stop_times.txt": StopTimes
+    })
+
+    # TO DO: Implemenet version checking
+    
+    FilesToGenerate.update({"feed_info.txt": 
+        JsonToCSVString({
+            "feed_publisher_name": "yes",
+            "feed_publisher_url": "yes",
+            "feed_lang": "ron",
+            "feed_contact_url": "yes",
+            #"feed_version": str(Version)
+        })
+    })
+    
+    # Generate gtfs zip archive
+    GenerateZIP(f'{Agency["agency_name"]}', FilesToGenerate)
+
+    print("Generated GTFS file succesfully!")
+
 def main():
     print("Starting GTFS generation")
 
@@ -119,58 +191,10 @@ def main():
 
     # Executing agency specific requestss
     for Agency in Agencies:
-        # Temporary bypass, Oradea can't be used but is in API
-        if Agency["agency_id"] == 9:
-            continue
-
-        # Fix agency URLs
-        Agency = FixAgency(Agency)
-
-        # Value to contain all csv formated strings
-        FilesToGenerate = {}
-
-        Routes = JsonToCSVString(FixRoute(FetchFromURL(f"{BASE_URL}/routes", {"X-Agency-Id" : str(Agency["agency_id"])})))
-        Trips = FetchFromURL(f"{BASE_URL}/trips", {"X-Agency-Id" : str(Agency["agency_id"])})
-        
-        # Generate shapes for each trip
-        print("Start shape generation")
-
-        Shapes = json.loads('[]')        
-        for Trip in tqdm(Trips, desc="Fetching shapes", unit="shape"):        
-            TripShape = FetchFromURL(f"{BASE_URL}/shapes", {"X-Agency-Id" : str(Agency["agency_id"])}, {"shape_id": Trip["shape_id"]})
-            Shapes += TripShape
-        
-        # Convert used jsons to csv format
-        Trips = JsonToCSVString(Trips)
-        Shapes = JsonToCSVString(Shapes)
-
-        Stops = JsonToCSVString(FetchFromURL(f"{BASE_URL}/stops", {"X-Agency-Id" : str(Agency["agency_id"])}))
-        StopTimes = JsonToCSVString(FetchFromURL(f"{BASE_URL}/stop_times", {"X-Agency-Id" : str(Agency["agency_id"])}))
-
-        # Gather files for compression
-        FilesToGenerate.update({
-            "agency.txt": JsonToCSVString(Agency),
-            "routes.txt": Routes,
-            "trips.txt": Trips,
-            "stops.txt": Stops,
-            "shapes.txt": Shapes,
-            "stop_times.txt": StopTimes
-        })
-
-        # TO DO: Implemenet version checking
-        
-        FilesToGenerate.update({"feed_info.txt": 
-            JsonToCSVString({
-                "feed_publisher_name": "yes",
-                "feed_publisher_url": "yes",
-                "feed_lang": "ron",
-                "feed_contact_url": "yes",
-                #"feed_version": str(Version)
-            })
-        })
-        
-        # Generate gtfs zip archive
-        GenerateZIP(f'{Agency["agency_name"]}', FilesToGenerate)
+        try:
+            ProccessAgency(Agency)
+        except Exception as e:
+            print(f"Failed to generate GTFS files for {Agency['agency_name']}, an error occured: {e}")
 
 if __name__ == "__main__":
     main()
